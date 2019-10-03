492ffc8e178a3661560ee8a13e88569a07393b05
DZTDEV <graveinsabre@gmail.com>
2019-10-03 21:05:45 +0700

    initial

---
+++ src/Views/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+export class Home extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+export default Home
